4bf4e66af762a5d5fc8136d5412c1484ac13f6c9
Stephen <stephen.ancliffe@gmail.com>
2026-01-13 15:53:14 -0500

    Add firewall rules for HTTP and HTTPS traffic

--- 1-gcp-setup/2-open-firewall.sh
+++ 1-gcp-setup/2-open-firewall.sh
@@ -10,3 +10,25 @@ gcloud compute instances add-tags $VM_NAME \
     --project=$PROJECT_ID \
     --zone=$ZONE \
     --tags=http-server,https-server
+    
+gcloud compute firewall-rules create allow-http \
+    --project=$PROJECT_ID \
+    --description="Incoming http allowed from anywhere" \
+    --direction=INGRESS \
+    --priority=1000 \
+    --network=default \
+    --action=ALLOW \
+    --rules=tcp:80 \
+    --source-ranges=0.0.0.0/0 \
+    --target-tags=http-server
+
+gcloud compute firewall-rules create allow-https \
+    --project=$PROJECT_ID \
+    --description="Incoming http allowed from anywhere" \
+    --direction=INGRESS \
+    --priority=1000 \
+    --network=default \
+    --action=ALLOW \
+    --rules=tcp:443 \
+    --source-ranges=0.0.0.0/0 \
+    --target-tags=http-server
